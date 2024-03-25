404bef6e1a0a76b57edcdbebcc219e3c1abf5f9f
Martin521 <29605222+Martin521@users.noreply.github.com>
2024-03-25 13:06:40 +0000

    Got Antlr4Templates example running with F#

---
+++ Driver/Program.fs
@@ -0,0 +1,36 @@
+﻿open Antlr4.Runtime
+open Parser
+open System.IO
+
+type ErrorListener<'T>() =
+    inherit ConsoleErrorListener<'T>()
+    let mutable hadError = false
+    member x.HadError = hadError
+    override _.SyntaxError(output: TextWriter, recognizer: IRecognizer, offendingSymbol: 'T, line: int,
+                            col: int, msg: string, e: RecognitionException) =
+        hadError <- true;
+        base.SyntaxError(output, recognizer, offendingSymbol, line, col, msg, e);
+
+
+let Try (input: string) =
+    let str = new AntlrInputStream(input)
+    System.Console.WriteLine(input)
+    let lexer = new ArithmeticLexer(str)
+    let tokens = new CommonTokenStream(lexer)
+    let parser = new ArithmeticParser(tokens)
+    let listener_lexer = new ErrorListener<int>()
+    let listener_parser = new ErrorListener<IToken>()
+    lexer.RemoveErrorListeners()
+    parser.RemoveErrorListeners()
+    lexer.AddErrorListener(listener_lexer)
+    parser.AddErrorListener(listener_parser)
+    let tree = parser.file()
+    if (listener_lexer.HadError || listener_parser.HadError) then
+        System.Console.WriteLine("error in parse.")
+    else
+        System.Console.WriteLine("parse completed.")
+    System.Console.WriteLine(tree.ToStringTree(parser))
+
+Try("1 + 2 + 3")
+Try("1 2 + 3")
+Try("1 + +")
--- README.md
+++
@@ -1,5 +0,0 @@
-# fsharp.antlr
-
-For now, I am trying to understand ANTLR and its potential in the context of the F# spec.
-
-This might develop into a project to create a F# ANTLR grammar.
